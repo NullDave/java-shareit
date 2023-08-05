ed39ade71afb543bc6faaaeda2e92a80775b5030
David <kolya.noskov2015@yandex.ru>
2023-08-05 23:02:11 +0300

    restyle: delete unused import

--- src/main/java/ru/practicum/shareit/exception/ExceptionApiHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ExceptionApiHandler.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.exception;
 
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
